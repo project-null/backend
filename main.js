1b2a813099607c18da9f3e663677407ae128e560
feng <fefengid@163.com>
2018-03-12 14:59:43 +0800

    整合

--- main.js
+++ main.js
@@ -1,39 +1,76 @@
-// const Koa = require('koa');
 import Koa from 'koa';
 import path from 'path';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
 
-const app = new Koa();
-
-app.use(logger())
-
-// swagger start
 import serve from 'koa-static-server';
-app.use(serve({ rootDir: 'swagger', rootPath: '/swagger' }))
-// swagger end
-
-// 解析body
-app.use(bodyParser());
-
-// 加载业务逻辑路由路由 start 
 import router from './src/routers/index.js';
-router(app);
-// 加载业务逻辑路由路由 end 
-
-
-// 初始化mongodb数据库并增加到context中
-import dbClient from './src/models/lib/mongodb.js';
-dbClient.connect();
-
-// 读取配置文件
 import util from './src/common/util.js';
-app.context.config = util.getConfig();
+import dbClient from './src/models/lib/mongodb.js';
 
+import etag from 'koa-etag';
+import  conditional from 'koa-conditional-get';
+import { Server } from 'net';
 
-// import graphql from './src/routers/graphql';
-// graphql(app);
+class StartUp {
+    constructor() {
+        this.app = new Koa();
+    }
+    init() {
+        this.logger();
+        this.bodyParser();
 
+        this.swagger();
+        this.initRouter();
+        this.fileServer();
+        this.frontEnd();
+        
+        this.getConfig();
+        this.initDB();
+        this.listen();
+    }
+    // 输出运行日志
+    logger() {
+        this.app.use(logger());
+    }
+    // 静态文件目录
+    fileServer(){
+        this.app.use(serve({ rootDir: 'static/files', rootPath: '/files' }));
+    }
+    // 前端目录
+    frontEnd(){
+        this.app.use(conditional());
+        this.app.use(etag());
+        this.app.use(serve({ rootDir: 'static/ui', rootPath: '/' }));
+    }
+    // swagger服务
+    swagger() {
+        this.app.use(serve({ rootDir: 'static/swagger', rootPath: '/swagger' }))
+    }
+    // 解析body内容
+    bodyParser() {
+        this.app.use(bodyParser());
+    }
+    
+    // 初始化API
+    initRouter() {
+        router(this.app);
+    }
+    // 获取配置信息
+    getConfig() {
+        this.util = util;
+        this.app.context.config = util.getConfig();
+    }
+    // 初始化数据库并连接数据库
+    initDB() {
+        dbClient.connect();
+    }
+    // 启动http服务
+    listen() {
+        console.info(`server port : ${this.util.getConfig().port}`);
+        this.app.listen(this.util.getConfig().port);
+    }
+}
 
-console.info(`server port : ${util.getConfig().port}`);
-app.listen(util.getConfig().port); 
+let startup = new StartUp();
+startup.init();
--- src/routers/index.js
+++ src/routers/index.js
@@ -5,37 +5,68 @@ import accountController from '../controllers/accounts';
 import commonController from '../controllers/common';
 
 import loginInfo from '../models/loginInfo';
+
+import fs from 'fs';
+import path from 'path';
+
+
+let indexStr = null;
 const tokenSurvivalTime = 30;
-const checkToken = async (ctx, next) => {
-
-    const ignoreURL = [
-        '/v1/users/login',
-        '/v1/users/registry',
-        '/graphql?',
-        '/graphiql',
-    ]
-
-    if (ignoreURL.indexOf(ctx.request.url) !== -1) {
-        return next(ctx);
-    }
-    let token = ctx.request.header.token;
-    if (!!token) {
-        let tokenInfo = await loginInfo.check(token, tokenSurvivalTime);
-        ctx.userInfo = tokenInfo;
-        if (!!tokenInfo) {
-            return next(ctx);
-        }        
-    }
-    ctx.status = 401
-    ctx.body = '当前token无效，请重新登陆';
-}
-
 setInterval(() => {
     loginInfo.checkTokens(tokenSurvivalTime);
 }, 1000 * 60)
 
+const checkToken = async (ctx, next) => {
+    let url = ctx.request.url;
+
+    if (/^\/v1\/.*/.test(url) === true) {
+        const ignoreURL = [
+            '/v1/users/login',
+            '/v1/users/registry',
+        ];
+        
+        
+        if (ignoreURL.indexOf(url) !== -1) {
+            return next(ctx);
+        }
+    
+        let token = ctx.request.header.token;
+        if (!!token) {
+            let tokenInfo = await loginInfo.check(token, tokenSurvivalTime);
+            ctx.userInfo = tokenInfo;
+            if (!!tokenInfo) {
+                return next(ctx);
+            }
+        }
+        ctx.status = 401
+        ctx.body = '当前token无效，请重新登陆';
+    }else{
+        return next(ctx);
+    }
+}
+
+const staticFileServer = async (ctx, next) => {
+    let url = ctx.request.url;
+
+    if (/^\/v1\/.*/.test(url) === false) {
+        if ((/.*(html|js|svg|css|png|woff2|woff|ttf|jpg|ico|json|yaml)$/).test(url)) {
+            return next(ctx);
+        } else {
+            if (indexStr == null) {
+                let indexPath = path.join(__dirname, '..', '..', 'static', 'ui', 'index.html');
+                indexStr = await fs.readFileSync(indexPath).toString();
+            }
+            ctx.body = indexStr;
+            ctx.status = 200
+        }
+    }else{
+        return next(ctx);
+    }
+}
+
 export default (app) => {
     app.use(checkToken);
+    app.use(staticFileServer);
     app.use(usersController.routes());
     app.use(favoritesFolderController.routes());
     app.use(favoritesController.routes());
